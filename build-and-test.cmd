24563136039fd5e0daaa7d07a15a755310c306cc
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-03-02 17:18:49 -0800

    add antlr grammar

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,3 +1,17 @@
-set TEST_PROJECT=.\src\IxMilia.Step.Test\IxMilia.Step.Test.csproj
-dotnet restore %TEST_PROJECT%
-dotnet test %TEST_PROJECT%
+@echo off
+set thisdir=%~dp0
+
+:: download antlr
+set antlr_jar=antlr-4.6-complete.jar
+mkdir "%thisdir%bin">NUL 2>&1
+if not exist "%thisdir%bin\%antlr_jar%" (
+    bitsadmin /transfer "Download Antlr" http://www.antlr.org/download/%antlr_jar% "%thisdir%bin\%antlr_jar%"
+)
+
+set CLASSPATH=.;%thisdir%bin\%antlr_jar%;%CLASSPATH%
+java org.antlr.v4.Tool %thisdir%src\ExpressSchema.g
+
+:: build
+set TEST_PROJECT=%thisdir%src\IxMilia.Step.Test\IxMilia.Step.Test.csproj
+rem dotnet restore %TEST_PROJECT%
+rem dotnet test %TEST_PROJECT%
