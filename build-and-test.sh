ae28deec7b5dac4a905fc7582eeb536557f1e1dc
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-03-28 11:12:23 -0700

    run schema parser tests on non-Windows

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
-TEST_PROJECT=./src/IxMilia.Step.Test/IxMilia.Step.Test.csproj
-dotnet restore $TEST_PROJECT
-dotnet test $TEST_PROJECT
+build_and_test () {
+    TEST_PROJECT=$1
+    dotnet restore $TEST_PROJECT
+    dotnet test $TEST_PROJECT
+}
+
+build_and_test ./src/IxMilia.Step.SchemaParser.Test/IxMilia.Step.SchemaParser.Test.fsproj
+build_and_test ./src/IxMilia.Step.Test/IxMilia.Step.Test.csproj
